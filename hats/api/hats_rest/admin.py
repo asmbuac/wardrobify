4af4e42487de773cbb998dd15adc4a8c9c4e8bff
Shayne <asmbuac@gmail.com>
2023-03-03 14:30:34 -0800

    idk what

--- hats/api/hats_rest/admin.py
+++ hats/api/hats_rest/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Hat
+
+@admin.register(Hat)
+class HatAdmin(admin.ModelAdmin):
+    pass
--- hats/api/hats_rest/urls.py
+++ hats/api/hats_rest/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import api_hats
+from .views import api_hats, api_hat
 
 
 urlpatterns = [
     path("hats/", api_hats, name="api_hats"),
-    path("hats/<int:pk>/", api_hats, name="api_hats"),
+    path("hats/<int:pk>/", api_hat, name="api_hat"),
 ]
